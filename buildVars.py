6e82ebdbb969ba308c828322ac5d55d52e5e8259
zstanecic <zvonimirek222@yandex.com>
2020-08-29 19:14:47 +0300

    added the ability to build addon using scons, generating translations using scons, * initial language files

---
+++ buildVars.py
@@ -0,0 +1,50 @@
+# -*- coding: UTF-8 -*-
+
+# Build customizations
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+# Full getext (please don't change)
+_ = lambda x: x
+
+# Add-on information variables
+addon_info = {
+	# for previously unpublished addons, please follow the community guidelines at:
+	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guidelines.txt
+	# add-on Name, internal for nvda
+	"addon_name": "YandexTranslate",
+	# Add-on summary, usually the user visible name of the addon.
+	# Translators: Summary for this add-on to be shown on installation and add-on information.
+	"addon_summary": _("Yandex translator for NVDA"),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+	"addon_description": _("""Translates text using service Yandex Translate."""),
+	# version
+	"addon_version": "2020.02.25",
+	# Author(s)
+	"addon_author": u"alekssamos <aleks-samos@yandex.ru>",
+	# URL for the add-on documentation support
+	"addon_url": "https://https://github.com/alexsamos/yandextranslate",
+	# Documentation file name
+	"addon_docFileName": None,
+	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
+	"addon_minimumNVDAVersion": 2019.3,
+	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
+	"addon_lastTestedNVDAVersion": 2020.2,
+	# Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
+	"addon_updateChannel": None,
+}
+
+
+import os.path
+
+# Define the python files that are the sources of your add-on.
+# You can use glob expressions here, they will be expanded.
+pythonSources = [os.path.join("addon", "*.py"),
+				 os.path.join("addon", "globalPlugins", "yandexTranslate", "*.py")]
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources = pythonSources + ["buildVars.py"]
+
+# Files that will be ignored when building the nvda-addon file
+# Paths are relative to the addon directory, not to the root directory of your addon sources.
+excludedFiles = []
