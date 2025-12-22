b6c44db14d082998b057735530506509e0e94fa9
alkssamos <aleks-samos@yandex.ru>
2025-12-22 14:55:35 +0300

    update

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Translates text using service Yandex Translate."""),
 	# version
-	"addon_version": "2025.10.15",
+	"addon_version": "2025.12.22",
 	# Author(s)
 	"addon_author": "alekssamos <aleks-samos@yandex.ru>",
 	# URL for the add-on documentation support
